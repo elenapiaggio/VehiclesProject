595c0a666a4ff42bfae6c942a1134b845ba56486
Elena Piaggio <elenapiaggio@gmail.com>
2020-06-09 14:24:58 +0200

    ADD phase 3

--- VehiclesProject/src/com/vehicles/project/Bike.java
+++ VehiclesProject/src/com/vehicles/project/Bike.java
@@ -1,8 +1,33 @@
 package com.vehicles.project;
 
+import java.util.List;
+
 public class Bike extends Vehicle {
 
   public Bike(String plate, String brand, String color) {
   	super(plate, brand, color);
+  }
+
+  public void addWheels(List<Wheel> frontWheels, List<Wheel> backWheels) throws Exception {
+    addWheel(frontWheels);
+    addWheel(backWheels);
+  }
+
+  public void addWheel(List<Wheel> wheels) throws Exception {
+    if (wheels.size() != 1)
+      throw new Exception();
+
+    Wheel wheel = wheels.get(0);
+    this.wheels.add(wheel);
+  }
+
+  @Override
+  public String toString() {
+    return "Bike{" +
+            "plate='" + plate + '\'' +
+            ", brand='" + brand + '\'' +
+            ", color='" + color + '\'' +
+            ", wheels=" + wheels +
+            '}';
   }
 }
--- VehiclesProject/src/com/vehicles/project/Main.java
+++ VehiclesProject/src/com/vehicles/project/Main.java
@@ -7,42 +7,84 @@ import java.util.regex.*;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-
-    String brand = "";
-    String color = "";
-
+    int option = 0;
+    boolean error = false;
     Scanner sc = new Scanner(System.in);
-    System.out.print("Plate: \t");
-    String plate = sc.nextLine();
 
+    do{
+      System.out.print("Do you want to create: \t (1) Car \t (2) Bike \t");
+      option = sc.nextInt();
+      switch (option){
+        case 1: {
+          createCar();
+          error = false;
+        }
+        break;
+        case 2: {
+          createBike();
+          error = false;
+        }
+        break;
+        default: error = true;
+          System.out.println("Incorrect option");
+      }
+    }
+    while (error);
+  }
+
+  public static boolean plateIsCorrect(String plate){
     String patPlate = "^[a-zA-Z]{4}[0-9]{2,3}";
     Pattern pat = Pattern.compile(patPlate);
     Matcher mat = pat.matcher(plate);
 
     if (mat.matches()) {
-      System.out.print("Brand: \t");
-      brand = sc.nextLine();
-
-      System.out.print("Color: \t");
-      color = sc.nextLine();
+      return true;
     } else {
-      System.out.println("Incorrect plat");
-      return;
+      return false;
     }
+  }
+
+  public static void createCar() throws Exception {
+    String brand = "";
+    String color = "";
+    String plate = "";
+    double diameterWheelBack = 0;
+    double diameterWheelFront = 0;
+    boolean plateIsCorrect = true;
+    boolean diameterIsCorrect = true;
+    Scanner sc = new Scanner(System.in);
+    System.out.println(" ******** Data for Car: ******** ");
+
+    do {
+      System.out.print("Plate: \t");
+      plate = sc.nextLine();
+      plateIsCorrect = plateIsCorrect(plate);
+      if(plateIsCorrect){
+        plateIsCorrect=true;
+      } else{
+        System.out.println("Incorrect plate, try again");
+      }
+    }
+    while(!plateIsCorrect);
+
+    System.out.print("Brand: \t");
+    brand = sc.nextLine();
+
+    System.out.print("Color: \t");
+    color = sc.nextLine();
 
     Car clientCar = new Car(plate, brand, color);
-
     System.out.println("\n*********** For Back Wheels ***********");
     System.out.print("Please, write the Brand: \t");
     String brandWheelBack = sc.nextLine();
 
-    System.out.print("Please, write the Diameter: \t");
-    double diameterWheelBack = sc.nextDouble();
-
-    if (!(diameterWheelBack >= 0.4 && diameterWheelBack < 4)) {
-      System.out.println("Incorrecto diameter!!");
-      return;
+    do{
+      System.out.print("Diameter: \t");
+      diameterWheelBack = sc.nextDouble();
+      diameterIsCorrect = diameterIsCorrect(diameterWheelBack);
     }
+    while(!diameterIsCorrect);
+
     Wheel backWheel1 = new Wheel(brandWheelBack, diameterWheelBack);
     Wheel backWheel2 = new Wheel(brandWheelBack, diameterWheelBack);
     List<Wheel> backWheelsList = new ArrayList<Wheel>();
@@ -54,13 +96,14 @@ public class Main {
     Scanner console = new Scanner(System.in);
     String brandWheelFront = console.nextLine();
 
-    System.out.print("Please, write the Diameter: \t");
-    double diameterWheelFront = console.nextDouble();
+    diameterIsCorrect = false;
 
-    if (!(diameterWheelFront >= 0.4 && diameterWheelFront < 4)) {
-      System.out.println("Incorrecto diameter!!");
-      return;
+    do{
+      System.out.print("Diameter: \t");
+      diameterWheelFront = sc.nextDouble();
+      diameterIsCorrect = diameterIsCorrect(diameterWheelFront);
     }
+    while(!diameterIsCorrect);
 
     Wheel frontWheel1 = new Wheel(brandWheelFront, diameterWheelFront);
     Wheel frontWheel2 = new Wheel(brandWheelFront, diameterWheelFront);
@@ -69,7 +112,86 @@ public class Main {
     frontWheelsList.add(frontWheel1);
 
     clientCar.addWheels(frontWheelsList, backWheelsList);
+    System.out.println("\nCar created correctly. Summary:");
     System.out.println(clientCar.toString());
+  }
+
+  public static void createBike() throws Exception {
+    String brand = "";
+    String color = "";
+    String plate = "";
+    double diameterWheelBack = 0;
+    double diameterWheelFront = 0;
+    boolean plateIsCorrect = true;
+    boolean diameterIsCorrect = false;
+
+    System.out.println(" ******** Data for Bike ******** ");
+    Scanner sc = new Scanner(System.in);
+
+    do {
+      System.out.print("Plate: \t");
+      plate = sc.nextLine();
+      plateIsCorrect = plateIsCorrect(plate);
+      if(plateIsCorrect){
+        plateIsCorrect=true;
+      } else{
+        System.out.println("Incorrect plate, try again");
+      }
+    }
+    while(!plateIsCorrect);
+
+    System.out.print("Brand: \t");
+    brand = sc.nextLine();
+
+    System.out.print("Color: \t");
+    color = sc.nextLine();
+
+    Bike clientBike = new Bike(plate, brand, color);
+
+    System.out.println("\n*********** For Back Wheel ***********");
+    System.out.print("Brand: \t");
+    String brandWheelBack = sc.nextLine();
+
+    do{
+      System.out.print("Diameter: \t");
+      diameterWheelBack = sc.nextDouble();
+      diameterIsCorrect = diameterIsCorrect(diameterWheelBack);
+    }
+    while(!diameterIsCorrect);
+
+    Wheel backWheel = new Wheel(brandWheelBack, diameterWheelBack);
+    List<Wheel> backWheelsList = new ArrayList<Wheel>();
+    backWheelsList.add(backWheel);
+
+    System.out.println("\n*********** For Front Wheel ***********");
+    System.out.print("Brand: \t");
+    Scanner console = new Scanner(System.in);
+    String brandWheelFront = console.nextLine();
+
+    do{
+      System.out.print("Diameter: \t");
+      diameterWheelFront = sc.nextDouble();
+      diameterIsCorrect = diameterIsCorrect(diameterWheelFront);
+    }
+    while(!diameterIsCorrect);
+
+    Wheel frontWheel = new Wheel(brandWheelFront, diameterWheelFront);
+    List<Wheel> frontWheelsList = new ArrayList<Wheel>();
+    frontWheelsList.add(frontWheel);
+
+    clientBike.addWheels(frontWheelsList, backWheelsList);
+
+    System.out.println("\nByke created correctly. Summary: ");
+    System.out.println(clientBike.toString());
+  }
+
+  public static boolean diameterIsCorrect(double diameter){
+    if (!(diameter >= 0.4 && diameter < 4)) {
+      System.out.print("\nIncorrect diameter, try again!!!\n");
+      return false;
+    } else{
+      return true;
+    }
   }
 
 }
